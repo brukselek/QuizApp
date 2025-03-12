2c27ab3a85b2fd9805065a03195a829bc6ce377b
brukselek <117040452+brukselek@users.noreply.github.com>
2025-03-12 12:04:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function themeColor() {
-   //kolor tla dla theme switcher
-   var bodyColor = document.body;
-   //ikona zmiany tla dla theme switchera
-   let icon = document.getElementById("themeIcon");
-   //toggle -> przelaczenie aktywnej klasy CSS
-bodyColor.classList.toggle("light-mode");
-      if(bodyColor.classList.contains("light-mode")) {
-         document.getElementById("themeIcon").src = "light_mode.png";
-      } else {
-         document.getElementById("themeIcon").src = "dark_mode.png";
-      }
-   }
-
-   //rozpoczecie segmentu QUIZU!
+//rozpoczecie segmentu QUIZU!
 
 /*tablica zawierajaca obiekty dla pytan do quizu*/
 const questions = [
@@ -41,17 +27,77 @@ const questions = [
    }
 ];
 
-//progres quizu
+//zmienna przechowujaca aktualny progres quizu
 const quizProgress = document.getElementById("quizProgress");
-//tresc pytania
+//zmienna przechowujaca tresc i temat pytania
 const questionContainer = document.getElementById("questionContainer");
-//odpowiedzi
+//zmienna przechowujaca odpowiedzi do pytania
 const answerContainer = document.getElementById("answerContainer");
+//zmienna przechowujaca obecny index pytania
+let currentQuestionIndex = 0;
 
-function handleQuestion(){
-   questions.forEach(question => {  
+function handleQuestion(index){
+
+   //handling sekcji progresu quizu
+   quizProgress.innerHTML = "";
+   questions.forEach((question) => {  
       quizProgress.innerHTML += "<span></span>";
    });
-}
+   /*zmienna przechowujaca wartosc wszystkich tagow <span> dla kontenerow o id = quizProgress*/
+   let spans = document.querySelectorAll("#quizProgress span");
+   /*dla aktywnego indexu pytania dodaje klase "seen" czyli widziane juz przez nas pytanie*/
+   for(let i = 0; i <= index; i++){
+      spans[i].classList.add("seen");
+   }
 
-handleQuestion();
+   //wyswietlanie aktualnego tematu/pytania
+   questionContainer.innerHTML = 
+   /*interpolacja stringow wyswietlajacych 
+   pytanie/temat (uzywamy ` backtickow!! a nie ' apostrofow)*/
+   `
+   <p>${questions[index].topic}</p>
+   <p>${questions[index].question}</p>
+   `;
+
+   //wyswietlanie mozliwych odpowiedzi
+   answerContainer.innerHTML = "";
+   questions[index].possibleAnswers.forEach((answer) => {
+      answerContainer.innerHTML += `<button>${answer}</button>`;
+   });
+   let answers = document.querySelectorAll("#answerContainer button");
+   answers.forEach((answer) => {
+      answer.addEventListener("click", (e) => {
+         if(e.target.textContent === questions[index].correctAnswer){
+            console.log("poprawna odpowiedz");
+         }
+         else{
+            console.log("zla odpowiedz");
+         }
+         if(currentQuestionIndex === questions.length - 1){
+            currentQuestionIndex = 0;
+         }
+         else{
+            currentQuestionIndex++;
+         }
+         handleQuestion(currentQuestionIndex);
+      });
+   });
+}
+handleQuestion(currentQuestionIndex);
+
+//segment do zmiany motywu
+function themeColor() {
+   //kolor tla dla theme switcher
+   var bodyColor = document.body;
+   //ikona zmiany tla dla theme switchera
+   let icon = document.getElementById("themeIcon");
+   //toggle -> przelaczenie aktywnej klasy CSS
+   
+   bodyColor.classList.toggle("light-mode");
+      if(bodyColor.classList.contains("light-mode")) {
+         document.getElementById("themeIcon").src = "light_mode.png";
+
+      } else {
+         document.getElementById("themeIcon").src = "dark_mode.png";
+      }
+   }
